fb29a18cf1f8e142bdbd1c32f0dbadcb94a7dfb8
pablo ocampo <juanpabloocampoleon@gmail.com>
2024-07-04 12:02:04 -0500

    primer proyecto de practica

---
+++ Practica1.java
@@ -0,0 +1,18 @@
+/*
+ * Proyecto 1: Sistema de Gestión de Empleados
+Descripción: Crea un sistema para gestionar empleados en una empresa.
+
+Clases y Herencia:
+Empleado: Clase base con atributos encapsulados como nombre, ID, y salario.
+EmpleadoTiempoCompleto y EmpleadoTiempoParcial: Clases derivadas de Empleado con atributos adicionales específicos para empleados a tiempo completo y parcial.
+Funciones:
+Métodos para establecer y obtener los atributos de los empleados (usando getters y setters).
+Métodos para calcular el salario basado en el tipo de empleado.
+ * 
+ * 
+ */
+
+
+public class Practica1 {
+    
+}
